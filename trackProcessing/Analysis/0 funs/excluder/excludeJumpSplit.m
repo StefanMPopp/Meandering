e04bbd77459061eea868c995cb745a8fff0a2e9b
Stefan Popp <popp@email.arizona.edu>
2024-02-29 12:10:12 +0100

    Delete trackProcessing/Analysis directory

--- trackProcessing/Analysis/0 funs/excluder/excludeJumpSplit.m
+++
@@ -1,9 +0,0 @@
-function spDat = excludeJumpSplit(spDat,rule)
-% Jump splits tracks at insanely high speeds
-spInd = find(spDat.v>rule);
-ids = spDat.id(spInd);
-maxID = max(spDat.id);
-for i = 1:length(spInd)
-    currTrEnd = find(spDat.id==ids(i),1,'last'); % Index of last point of track to be split
-    spDat.id(spInd(i):currTrEnd) = maxID+i; % Assigns following part of track highest ID
-end
